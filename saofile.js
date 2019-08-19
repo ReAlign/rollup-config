3ce59f424e3a370adbb139eed7ba0035af15b761
ReAlign <realign@yeah.net>
2019-08-19 19:31:00 +0800

    Update and rename sao.js to saofile.js

---
+++ saofile.js
@@ -0,0 +1,40 @@
+const superb = require('superb')
+
+module.exports = {
+    prompts: {
+        lineName: {
+            message: `Name:`,
+            default: ':folderName:'
+        },
+        proDesc: {
+            message: `Description:`,
+            default: 'rollup pro'
+        },
+        hubBase: {
+            message: `github base url:`,
+            default: ''
+        },
+        author: {
+            message: `author name:`,
+            default: ''
+        }
+    },
+    actions: [
+        {
+          type: 'add',
+          // Copy and transform all files in `template` folder into output directory
+          files: '**'
+        },
+        {
+          type: 'move',
+          patterns: {
+            gitignore: '.gitignore'
+          }
+        }
+    ],
+    async completed() {
+        this.gitInit()
+        await this.npmInstall()
+        this.showProjectTips()
+    }
+};
